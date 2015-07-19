7e5611e65e33d947945719c046cb93b46348b731
Tom McClintock <tmcclintock89@gmail.com>
2015-07-18 20:27:02 -0700

    completed equations of motion source code. fixed a typo in constants.h. Still need a makefile to check compilation.

--- src/constants.h
+++ src/constants.h
@@ -6,5 +6,5 @@
 #define A 0.057 //planform area of a frisbee (top down); m^2
 #define d 0.2694//diameter of frisbee; m
 #define rho 1.23//average density of air; kg/m^3
-#define Iz 0.002352 //moment of inertia about ZZ; kg*m^2
-#define Iss0.001219 //moment of inertia about either XX or YY; kg*m^2
+#define Izz 0.002352 //moment of inertia about ZZ; kg*m^2
+#define Iss 0.001219 //moment of inertia about either XX or YY; kg*m^2
--- src/equations_of_motion.c
+++ src/equations_of_motion.c
@@ -13,6 +13,7 @@
      but could in principle account for a time-varying wind
    - 'params' contains all of the parameters we are fitting for. That is,
      all of the flight parameters that are unknown
+  -  'tau' refers to a torque (aka moment) along one of the inertial axes
 */
 
 double dot(double*A,double*B){
@@ -85,9 +86,9 @@ void equations_of_motion(double*positions,double*derivs,
   //the inertial frame (N) to the body frame (C)
   // the 
   double Tnc[3][3];
-  Tnc[0][0] = c_tht,  Tnc[0][1] = s_phi*s_tht, Tnc[0][2] = -c_phi*s_tht;
-  Tnc[1][0] = 0,      Tnc[1][1] = c_phi,       Tnc[1][2] = s_phi;
-  Tnc[2][0] = s_tht,  Tnc[2][1] = -s_phi*c_tht, Tnc[2][2] = c_phi*c_tht;
+  Tnc[0][0] = c_tht, Tnc[0][1] = s_phi*s_tht,  Tnc[0][2] = -c_phi*s_tht;
+  Tnc[1][0] = 0,     Tnc[1][1] = c_phi,        Tnc[1][2] = s_phi;
+  Tnc[2][0] = s_tht, Tnc[2][1] = -s_phi*c_tht, Tnc[2][2] = c_phi*c_tht;
 
   //Define the rotation matrix rows
   double C1[] = {Tnc[0][0],T[0][1],T[0][2]};
@@ -128,5 +129,102 @@ void equations_of_motion(double*positions,double*derivs,
   Flift[0] = Flift[0]*Flift_amp; Flift[1] = Flift[1]*Flift_amp; Flift[2] = Flift[2]*Flift_amp;
 
   //Calculate the drag force
-  
+  double Fdrag_amp = CD(alpha,CD0,CD1)*A*rho*dot(v,v)/2.;
+  double Fdrag[]={-Fdrag_amp*v_hat[0],-Fdrag_amp*v_hat[1],-Fdrag_amp*v_hat[2]};
+
+  //Calculate the gravitational force
+  double Fgrav[]={0,0,-m*g};
+
+  //Sum the forces to get the total force
+  double Ftot[] = {Flift[0]+Fdrag[0],Flift[1]+Fdrag[1],Flift[2]+Fdrag[2]+Fgrav[2]};
+
+  //Write the angular velocities in the body frame
+  double w_in_C[] = {phiDot*c_tht, thetaDot, phiDot*s_tht + gammaDot};
+
+  //Calculate the angular velocity in the inertial frame
+  double w_in_N[3];
+  for(i=0;i<3;i++){
+    temp1 = 0;
+    for(j=0;j<3;j++){//Tnc.T dot w_in_C
+      temp1+= Tnc[j][i] * w_in_C[j];
+    }
+    w_in_N[i] = temp1;
+  }
+
+  //Find the components of the angular velocity
+  //as expressed in the inertial frame along each of the body frame unit
+  //vectors
+  double w_x = dot(w_in_N,x_C_hat);
+  double w_y = dot(w_in_N,y_C_hat);
+  double w_z = dot(w_in_N,z_C_hat);
+
+  //Calculate the spin parameter, which could be used later
+  //to incorporate the Robins-Magnus forec or if we
+  //have more accurate data about drag coefficients
+  double spin_parameter = w_z*d/(2*norm_v);
+
+  //Calculate each torque (aka moment)
+  double tau_amp = A*rho*d*dot(v,v)/2;
+  double tau_x_amp = CR(w_x,w_z,CR1x,CR1z)*tau_amp;
+  double tau_y_amp = CM(alpha,w_y,CM0,CM1a,CM1y)*tau_amp;
+  double tau_z_amp = CN(w_z,CN1z)*tau_amp;
+  double tau_x[] = {x_C_hat[0]*tau_x_amp, x_C_hat[1]*tau_x_amp, x_C_hat[2]*tau_x_amp};
+  double tau_y[] = {y_C_hat[0]*tau_y_amp, y_C_hat[1]*tau_y_amp, y_C_hat[2]*tau_y_amp};
+  double tau_z_N[] = {0, 0, tau_z_amp};
+  //Note: tau_x and tau_y are not in the inertial frame (yet)
+
+  //Calculate tau_x_N and tau_y_N
+  double tau_x_N[3];
+  double tau_y_N[3];
+  for(i=0;i<3;i++){
+    temp1 = 0;
+    temp2 = 0;
+    for(j=0;j<3;j++){//Tnc.T dot w_in_C
+      temp1+= Tnc[i][j] * tau_x[j];
+      temp2+= Tnc[i][j] * tau_y[j];
+    }
+    tau_x_N[i] = temp1;
+    tau_y_N[i] = temp2;
+  }
+
+  //Calculate the total torque (aka moment) in the inertial frame
+  double tau_total[] = {tau_x_N[0]+tau_y_N[0],
+    tau_x_N[1]+tau_y_N[1],
+    tau_x_N[2]+tau_y_N[2]+tau_z_N[2]};
+
+  //If we want, set the torques all to 0
+  //tau_total[0] = 0, tau_total[1] = 0, tau_total[2] = 0;
+
+  //TODO: Remove the wind from the velocities
+  /*
+    vx = vx + v_wind_x;
+    vy = vy + v_wind_y;
+    vz = vz + v_wind_z;
+  */
+
+  //Calculate all of the derivatives
+  derivs[0] = vx;
+  derivs[1] = vy;
+  derivs[2] = vz;
+  derivs[3] = Ftot[0]/m;
+  derivs[4] = Ftot[1]/m;
+  derivs[5] = Ftot[2]/m;
+  derivs[6] = phiDot;
+  derivs[7] = thetaDot;
+  derivs[8] = (tau_total[0] 
+               + 2*Iss*thetaDot*phiDot*s_tht 
+               - Izz*thetaDot*(phiDot*s_tht+gammaDot)
+              )/(Iss*c_tht);
+  derivs[9] = (tau_total[1]
+               + Izz*phiDot*c_tht*(phiDot*s_tht+gammaDot)
+               - Iss*phiDot*phiDot*c_tht*s_tht
+              )/Iss;
+  derivs[10]= (tau_total[2]
+               - Izz*derivs[8]*s_tht
+               - Iss*thetaDot*phiDot*c_tht
+              )/Izz;
+  derivs[11]= gammaDot;
+
+  //End the function
+  return;
 }
