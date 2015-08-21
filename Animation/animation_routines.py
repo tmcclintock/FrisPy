found two bugs that were causing flights to be incorrect. Missing factor of 10 in CR1x and wrong sign in the equation for CM where CM0 was used

Animation/animation_routines.py
@@ -1,44 +1,8 @@
 import numpy as np
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
-
-def make_plots(positions, n_times):
-	print "\nNow running animation routines"
-
-	#Parse out all of the coordinates from the positions array
-	t = positions[:,0]
-	x = positions[:,1]
-	y = positions[:,2]
-	z = positions[:,3]
-	vx = positions[:,4]
-	vy = positions[:,5]
-	vz = positions[:,6]
-	phi = positions[:,7]
-	theta = positions[:,8]
-	phiDot = positions[:,9]
-	thetaDot = positions[:,10]
-	gammaDot = positions[:,11]
-	gamma = positions[:,12]
-
-	#Find the indices where the disc is still in the air
-	in_air_indexes = np.where(z>0)
-	print "Useful indicies: ",in_air_indexes[0][0]," through ",in_air_indexes[0][-1]
-
-	#for i in xrange(0,len(in_air_indexes[0])):
-	for i in xrange(0,len(t)):
-		#print x[i],y[i],z[i],t[i]
-		#print phi[i],theta[i]
-		print positions[i]
-
-	#Plot Z vs X
-	#plot_Z_vs_X(x,z,in_air_indexes)
-
-	#Plot phi(t) and theat(t)
-	#plot_angles(phi,theta,t,in_air_indexes)
-
-	#End the plotting
-	return
 
 def plot_Z_vs_X(x,z,ind):
 	print "\tPlotting Z vs X"
@@ -46,25 +10,74 @@ def plot_Z_vs_X(x,z,ind):
 	ax1 = plt.subplot(111)
 	ax1.plot(x[ind],z[ind])
 	ax1.set_ylim(0,max(z[ind]))
+	ax1.set_xlabel("X (m)")
+	ax1.set_ylabel("Z (m)")
+	plt.show()
+	return
+
+def plot_X_vs_Y(x,y,ind):
+	print "\tPlotting Y vs X"
+	fig = plt.figure()
+	ax1 = plt.subplot(111)
+	ax1.plot(x[ind],y[ind])
+	ax1.set_ylim(0,max(y[ind]))
+	ax1.set_xlabel("X (m)")
+	ax1.set_ylabel("Y (m)")
 	plt.show()
 	return
 
 def plot_angles(phi,theta,t,ind):
 	print "\tPlotting phi(t) and theta(t)"
-	radians = np.pi/180. #used to convert to radians
-	degrees = 180./np.pi #used to convert to degrees
+	max_phi = max(np.fabs(phi[ind]))
+	max_theta = max(np.fabs(theta[ind]))
 
 	fig = plt.figure()
 	plt.subplots_adjust(hspace=0.001)
 
 	ax1 = plt.subplot(211)
-	ax1.plot(t[ind],phi[ind]*degrees)
+	ax1.plot(t[ind],phi[ind])
 	ax1.set_xticklabels([])
-	ax1.set_ylim(-180,180)
+	ax1.set_ylim(-max_phi,max_phi)
+	ax1.set_xlabel("Time (s)")
+	ax1.set_ylabel(r"$\phi(t)$")
 
 	ax2 = plt.subplot(212)#,sharex=ax1)
-	ax2.plot(t,theta*degrees)
-	ax2.set_ylim(-90,90)
+	ax2.plot(t[ind],theta[ind])
+	ax2.set_ylim(-max_theta,max_theta)
+	ax2.set_xlabel("Time (s)")
+	ax2.set_ylabel(r"$\theta(t)$")
+
 	fig.tight_layout()
+	plt.show()
+	return
+
+def update_3D_trajectory(num,traj_lines,lines):
+	for line, data in zip(lines,traj_lines):
+		line.set_data(data[0:2,:num])
+		line.set_3d_properties(data[2,:num])
+	return line
+
+#This creates a 3D trajectory of the throw
+def plot_3D_trajectory(x,y,z,nsteps):
+	print "\tPlotting 3D trajectory"
+	mpl.rcParams['legend.fontsize'] = 10
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+	ax.set_xlim3d(min(x),max(x))
+	ax.set_ylim3d(min(y),max(y))
+	ax.set_zlim3d(0,max(z))
+	fig.tight_layout()
+	#The next line plots the trajectory immediately
+	#full_line = ax.plot(x,y,z,label="Flight path",c='pink')
+	lines = [ax.plot(x,y,z,label="Flight Path",c="b")[0],\
+			 ax.plot(x,y,z*0,c="g",linestyle='--')[0],\
+			 ax.plot(x,y*0+max(y),z,c="r",linestyle=":")[0]]
+	ax.set_xlabel('X (m)')
+	ax.set_ylabel('Y (m)')
+	ax.set_zlabel('Z (m)')
+	trajectory = np.array([[x,y,z],[x,y,z*0],[x,y*0+max(y),z]])
+	lines_animation = animation.FuncAnimation(fig,update_3D_trajectory,\
+			nsteps,fargs=(trajectory,lines),interval=0.0,blit=False)
+	#lines_animation.save('example_throw.gif',fps=30)
 	plt.show()
 	return

Animation/driver_interface_animation.py
@@ -15,18 +15,22 @@ def get_positions(initial_positions,coeffs):
 	coeffs = np.array(coeffs, dtype=np.double).copy()
 
 	#Convert the initial angles and angular velocities in degrees to radians
-	radians = np.pi/180.
-	initial_positions[7:] = initial_positions[7:]*radians
-	print initial_positions
+	radians = np.pi/180. #used to convert to radians
+	degrees = 180./np.pi #used to convert to degrees
+	initial_positions[6:] = initial_positions[6:]*radians
 
 	#Animations shouldn't have specified flight times,
 	#but since driver() takes one we specify a flight time
 	#that is way longer than anything we would expect
 	#a reasonable throw to take
-	flight_time = 1.0 #seconds
+	flight_time = 3.2 #seconds
+
+	#Set the timestep
+	#In the MCMC this will be smaller by a factor of at least 10
+	timestep = 0.01
 
 	#Number of time steps
-	n_times = 20
+	n_times = int(flight_time/timestep)
 
 	#Load the driver library and its funcitons
 	#Then specify the arguement and return types
@@ -49,9 +53,10 @@ def get_positions(initial_positions,coeffs):
 	co_out = coeffs.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
 	ap_out = all_positions.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
 
-	#Call the driver
+	#Call the driver and change back to degrees
 	driver(ip_out,co_out,flight_time,n_times,ap_out)
 	all_positions = np.array(ap_out[0:n_times*13]).reshape((n_times,13))
+	all_positions[:,6:]*=degrees
 
 	#Call the cleanup
 	#This is unnecessary since all_positions is declared in python

Old_Python_Animation/Animation.py
@@ -84,7 +84,7 @@ def main():
     speed0 = 14.0    #m/s; initial speed
     phi0  = 0.0   #degrees; roll angle (+ means OI BH/IO FH, - is opposite)
     tht0 = 0.0       #degrees; polar angle tilt (- means the throw is airbounce-y)
-    fpAng0 = 5.0       #degrees; flight path angle between v and x-lab
+    fpAng0 = 0.0       #degrees; flight path angle between v and x-lab
     gamDot0 = -50.0    #rad/s; spin on the disc (- is BH, + is FH)
     xyang = 00.0     #angle between x-lab axis and velocity. This is useful for plotting
     tf = 3.2  #s; final time
@@ -281,7 +281,6 @@ def eqOfMotion(params,t):
     vp = v - C3*vC3
 
     #Calculate the angle of attack, alpha (alf)
-    #TOM: I THINK that I want -atan, but it might be +atan
     alf = -math.atan(vC3/np.linalg.norm(vp))
 
     #Find some unit vectors.
@@ -446,7 +445,7 @@ def plotTrajectory(solution,nsteps):
     lines_ani = animation.FuncAnimation(fig, updateTrajectory,
                                        nsteps,fargs=(trajectory,lines),
                                        interval=0.0001,blit=False)
-
+    print "nsteps = ",nsteps
     #lines_ani.save(throwname+'throw.gif',fps=20)
     plt.show()
 