14442e9d5bcccc96f2cffa7c6df3a6c80533b112
Tom McClintock <tmcclintock@email.arizona.edu>
2015-11-19 12:14:27 -0700

    Commented the algorithm in the MCMC likelihood.

--- MCMC/mcmc_likelihood.py
+++ MCMC/mcmc_likelihood.py
@@ -1,6 +1,19 @@
 import numpy as np
+from scipy import interpolate.InterpolatedUnivariateSpline as IUS
 
 #This is a likelihood function that minimizes the percent difference
 #between the observed trajectory and the model trajectory
 def lnlike():
+    #Extract the parameters
+
+    #Make the model
+
+    #Isolate the trajectory that matches the data (i.e. get rid of angles)
+
+    #Create a spline for each of the coordinates as a function of the time
+
+    #Compare the model at each time in the data to the trajectory at that time
+
+    #Sum over the percent differences
+    
     return 0
