4426382aa58b98ab8b97ca46d3d0044617cad03e
Tom McClintock <tmcclintock@email.arizona.edu>
2015-11-20 13:11:33 -0700

    Continued to work on mcmc_likelihood.py. Added a driver interface for mcmc.

--- MCMC/mcmc_likelihood.py
+++ MCMC/mcmc_likelihood.py
@@ -1,12 +1,15 @@
 import numpy as np
 from scipy import interpolate.InterpolatedUnivariateSpline as IUS
+import driver_interface_mcmc
 
 #This is a likelihood function that minimizes the percent difference
 #between the observed trajectory and the model trajectory
-def lnlike():
+def lnlike(positions,params):
     #Extract the parameters
+    initial_positions,coeffs = params
 
     #Make the model
+    #model_trajectory,n_times = driver_interface_mcmc.get_positions(initial_positions,coeffs)
 
     #Isolate the trajectory that matches the data (i.e. get rid of angles)
 
