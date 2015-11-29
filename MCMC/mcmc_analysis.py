331b94da7fd1333f9f4d6d45a0651c2811ea3ff0
Tom McClintock <tmcclintock@email.arizona.edu>
2015-11-29 08:10:33 -0700

    Adding comments to mcmc_analysis.py

--- MCMC/mcmc_analysis.py
+++ MCMC/mcmc_analysis.py
@@ -3,4 +3,8 @@ import numpy as np
 def analysis(samples,likelihoods,names,flight_data,params):
     print "\nPerforing analysis of MCMC chain"
 
+    #Create corner plot
+
+    #Make 3D plot of trajectory and best fit trajectory
+
     return 0
