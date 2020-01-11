7ca05547c941fbc700ccd2c4ee2423b01fe0b3de
Tom McClintock <tmcclintock89@gmail.com>
2020-01-11 11:13:35 -0500

    Added linebreaks to help PEP8.

--- FrisPy/FrisPy.py
+++ FrisPy/FrisPy.py
@@ -10,17 +10,19 @@ class Disc(object):
     Discraft Ultrastars (175 grams or 0.175 kg).
 
     All masses are kg, lengths are meters (m) and times are seconds (s). That is,
-    these files all use `mks` units.
+    these files all use `mks` units. Angular units use radians (rad), and 
+    angular velocities are in rad/s.
 
-    air_density (float): default is 1.225 kg/m^3
-    area (float): default is 0.057 m^2
-    diameter (float): default is 0.27 m; (computed from default area)
-    g (float): default is 9.81 m/s^2; gravitational acceleration on Earth
-    grav_vector (numpy.ndarray): default is [0,0,-1]; is a unit vector
-    I_zz (float): default is 0.001219 kg*m^2; z-axis moment of inertia
-    I_xx (float): default is 0.175 kg*m^2; x and y-axis moments of inertia
-        (i.e. is the same as I_yy and the cross components I_xy)
-    mass (float): defualt is 0.175 kg
+    Args:
+        air_density (float): default is 1.225 kg/m^3
+        area (float): default is 0.057 m^2
+        diameter (float): default is 0.27 m; (computed from default area)
+        g (float): default is 9.81 m/s^2; gravitational acceleration on Earth
+        grav_vector (numpy.ndarray): default is [0,0,-1]; is a unit vector
+        I_zz (float): default is 0.001219 kg*m^2; z-axis moment of inertia
+        I_xx (float): default is 0.175 kg*m^2; x and y-axis moments of inertia
+            (i.e. is the same as I_yy and the cross components I_xy)
+        mass (float): defualt is 0.175 kg
 
     """
     def __init__(self, **kwargs):
@@ -31,9 +33,11 @@ class Disc(object):
                           np.array([0,0,-1]), 0.002352, 0.001219, 0.175]
         default_values.insert(2, 2*(default_values[1]/np.pi)**0.5)
         # initialize all allowed keys to defaults
-        self.__dict__.update((key, value) for key, value in zip(allowed_keys, default_values))
+        self.__dict__.update((key, value) for key, value in
+                             zip(allowed_keys, default_values))
         # and update the given keys by their given values
-        self.__dict__.update((key, value) for key, value in kwargs.items() if key in allowed_keys)
+        self.__dict__.update((key, value) for key, value in
+                             kwargs.items() if key in allowed_keys)
 
         #Attach an uninitialized trajectory object
         self._trajectory = Trajectory()
