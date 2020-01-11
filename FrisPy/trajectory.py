f4c84c1ce9553ae4d73f2ae257d2cfd896ac2373
Tom McClintock <tmcclintock89@gmail.com>
2020-01-11 11:13:59 -0500

    Initialized the trajectory with default values.

--- FrisPy/trajectory.py
+++ FrisPy/trajectory.py
@@ -1,5 +1,27 @@
 import numpy as np
 
 class Trajectory(object):
+    """Trajectory of a disc object. This object contains the initial
+    values, and computed values for the kinematic variables of
+    the disc including the position, velocities, angles, and
+    angular velocities. Units are all given in `FrisPy.py` in
+    the documentation for the `Disc` object.
+
+    
+
+    """
     def __init__(self, **kwargs):
-        pass
+        allowed_keys = ['x', 'y', 'z', 'vx', 'vy', 'vz',
+                        'phi', 'theta', 'gamma',
+                        'phidot', 'thetadot', 'gammadot']
+        default_values = [0, 0, 1, 10, 0, 0,
+                          0, 0, 0, 0, 0, 50]
+        # initialize all allowed keys to defaults
+        self.__dict__.update((key, value) for key, value in
+                             zip(allowed_keys, default_values))
+        # and update the given keys by their given values
+        self.__dict__.update((key, value) for key, value in
+                             kwargs.items() if key in allowed_keys)
+
+        self.initial_values = {"initial_"+key: value for key, value in
+                               self.__dict__.items() if key in allowed_keys}
