4e9ca86be1e81d679ea23e6c1845bf8bf56a6eaa
Tom McClintock <tmcclintock89@gmail.com>
2020-12-06 16:30:22 -0800

    Implemented initial conditions on the disc.

--- frispy/disc.py
+++ frispy/disc.py
@@ -1,8 +1,4 @@
-"""
-Simulations of a flying disc. This file contains the disc object.
-
-Physics are carried out elsewhere.
-"""
+from typing import Dict, Optional
 
 from frispy.equations_of_motion import EOM
 from frispy.environment import Environment
@@ -21,12 +17,25 @@ class Disc:
     angular velocities are in rad/s.
 
     Args:
-        environment (Environment): experiment environment that contains e.g.
-            air density, disc mass, etc. See the ``Environment``. Default is
+      eom (EOM, optional): the equations of motion
+      initial_conditions (Dict, optional): initial conditions of the disc in
+        flight units are in in "mks" and angles are in radians. By defualt,
+        the initial conditions will be that the disc is 1 meter off the
+        ground (`z=1`), moving at 10 meters/sec in the `x` direction
+        (`vx=10`) and is spinning about it's vertical axis at approximately
+        10 revolutions per second (approx. 62 rad/sec, or `dgamma=62`).
+        All other kinematic variables are set to 0. This configuration
+        results in an angle of attack of 0.
     """
 
-    def __init__(self, eom=EOM()):
+    def __init__(
+        self,
+        eom: EOM = EOM(),
+        initial_conditions: Optional[Dict[str, float]] = None,
+    ):
         self._eom = eom
+        self.set_default_initial_conditions(initial_conditions)
+        self.reset_initial_conditions()
 
     def compute_trajectory(self):
         """Call the differential equation solver to compute
@@ -35,13 +44,51 @@ class Disc:
         """
         pass
 
+    def reset_initial_conditions(self) -> None:
+        """
+        Set the initial_conditions of the disc to the default and
+        clear the trajectory.
+        """
+        self.initial_conditions = self.default_initial_conditions
+        self.current_coordinates = self.initial_conditions.copy()
+        self.current_trajectory = None
+        return
+
+    def set_default_initial_conditions(
+        self, initial_conditions: Optional[Dict[str, float]]
+    ) -> None:
+        base_ICs = {
+            "x": 0,
+            "y": 0,
+            "z": 1.0,
+            "vx": 10.0,
+            "vy": 0,
+            "vz": 0,
+            "phi": 0,
+            "theta": 0,
+            "gamma": 0,
+            "dphi": 0,
+            "dtheta": 0,
+            "dgamma": 62.0,
+        }
+        for i in base_ICs:
+            if initial_conditions is not None:
+                assert (
+                    i in initial_conditions
+                ), f"{i} missing from initial conditions"
+        self._default_initial_conditions = initial_conditions or base_ICs
+
     @property
-    def eom(self) -> EOM:
-        return self._eom
+    def default_initial_conditions(self) -> Dict[str, float]:
+        return self._default_initial_conditions
 
     @property
     def environment(self) -> Environment:
         return self._eom.environment
+
+    @property
+    def eom(self) -> EOM:
+        return self._eom
 
     @property
     def model(self) -> Model:
--- tests/test_disc.py
+++ tests/test_disc.py
@@ -1,6 +1,7 @@
 """
 Tests of the ``Disc`` class.
 """
+import pytest
 
 from unittest import TestCase
 
@@ -8,10 +9,61 @@ from frispy import Disc
 
 
 class TestDisc(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.ics = {
+            "x": 0,
+            "y": 0,
+            "z": 1.0,
+            "vx": 10.0,
+            "vy": 0,
+            "vz": 0,
+            "phi": 0,
+            "theta": 0,
+            "gamma": 0,
+            "dphi": 0,
+            "dtheta": 0,
+            "dgamma": 62.0,
+        }
+
     def test_smoke(self):
         d = Disc()
         assert d is not None
 
-    def test_disc_has_trajectory(self):
+    def test_disc_has_properties(self):
         d = Disc()
-        assert hasattr(d, "_trajectory")
+        assert hasattr(d, "trajectory")
+        assert hasattr(d, "model")
+        assert hasattr(d, "environment")
+        assert hasattr(d, "eom")
+
+    def test_initial_conditions(self):
+        d = Disc()
+        assert d.initial_conditions == self.ics
+        assert d.current_coordinates == self.ics
+        assert d.current_trajectory is None
+        ics = self.ics.copy()
+        ics["x"] = 1.0
+        assert ics != self.ics
+        d = Disc(initial_conditions=ics)
+        assert d.initial_conditions == ics
+        assert d.current_coordinates == ics
+        assert d.current_trajectory is None
+
+    def test_reset_initial_conditions(self):
+        d = Disc()
+        d.current_trajectory = "blah"
+        d.reset_initial_conditions()
+        assert d.current_trajectory is None
+
+    def test_set_default_initial_conditions(self):
+        d = Disc()
+        assert d.default_initial_conditions == self.ics
+        ics = self.ics.copy()
+        ics["x"] = 1.0
+        d.set_default_initial_conditions(ics)
+        assert ics != self.ics
+        assert d.default_initial_conditions == ics
+        _ = ics.pop("x")
+        with pytest.raises(AssertionError):
+            d.set_default_initial_conditions(ics)
