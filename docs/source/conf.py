02d707da310c04ff0bdd959dc562745baf6ec7b5
Tom McClintock <tmcclintock89@gmail.com>
2020-12-13 21:08:21 -0800

    Autoapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,6 +29,7 @@ author = "Tom McClintock"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "autoapi.extension",
     "sphinx.ext.autodoc",
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
@@ -61,41 +62,6 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-# https://michaelgoerz.net/notes/extending-sphinx-napoleon-docstring-sections.html
-# -- Extensions to the  Napoleon GoogleDocstring class ---------------------
+# Autoapi settings
 
-from sphinx.ext.napoleon.docstring import GoogleDocstring  # noqa: E402
-
-
-# first, we define new methods for any new sections and add them to the class
-def parse_keys_section(self, section):
-    return self._format_fields("Keys", self._consume_fields())
-
-
-GoogleDocstring._parse_keys_section = parse_keys_section
-
-
-def parse_attributes_section(self, section):
-    return self._format_fields("Attributes", self._consume_fields())
-
-
-GoogleDocstring._parse_attributes_section = parse_attributes_section
-
-
-def parse_class_attributes_section(self, section):
-    return self._format_fields("Class Attributes", self._consume_fields())
-
-
-GoogleDocstring._parse_class_attributes_section = parse_class_attributes_section
-
-
-# we now patch the parse method to guarantee that the the above methods are
-# assigned to the _section dict
-def patched_parse(self):
-    self._sections["keys"] = self._parse_keys_section
-    self._sections["class attributes"] = self._parse_class_attributes_section
-    self._unpatched_parse()
-
-
-GoogleDocstring._unpatched_parse = GoogleDocstring._parse
-GoogleDocstring._parse = patched_parse
+autoapi_dirs = ["../../frispy"]
