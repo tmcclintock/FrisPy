ec418b3e6f866cfb9312d8a332f4f5a0fe9ae28b
Tom McClintock <tmcclintock89@gmail.com>
2020-12-22 19:50:43 -0800

    Bumped version.

--- frispy/__init__.py
+++ frispy/__init__.py
@@ -5,5 +5,5 @@ from .model import Model
 from .trajectory import Trajectory
 
 __author__ = "Tom McClintock thmsmcclintock@gmail.com"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __docs__ = "Simulates flying spinning discs."
