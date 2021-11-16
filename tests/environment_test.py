9057f39b9d89876a2453c5af90c6bcfa3487dd1a
Tom McClintock <tmcclintock89@gmail.com>
2021-11-16 17:09:13 -0500

    Fixed environment tests

--- tests/environment_test.py
+++ tests/environment_test.py
@@ -1,29 +1,27 @@
 """
 Tests of the ``Environment`` object.
 """
-from unittest import TestCase
-
 import numpy as np
 
 from frispy import Environment
 
 
-class TestEnvironment(TestCase):
-    def test_smoke(self):
-        e = Environment()
-        assert e is not None
+def test_smoke():
+    e = Environment()
+    assert e is not None
 
-    def test_default_values(self):
-        e = Environment()
-        d = {
-            "air_density": 1.225,
-            "area": 0.57,
-            "g": 9.81,
-            "grav_unit_vector": np.array([0.0, 0.0, -1.0]),
-            "I_zz": 0.002352,
-            "I_xx": 0.001219,
-            "mass": 0.175,
-        }
-        for k, v in d.items():
-            np.testing.assert_equal(getattr(e, k), v, k)
-        assert (e.diameter / 2) ** 2 * np.pi == e.area
+
+def test_default_values():
+    e = Environment()
+    d = {
+        "air_density": 1.225,
+        "area": 0.058556,
+        "g": 9.81,
+        "grav_unit_vector": np.array([0.0, 0.0, -1.0]),
+        "I_zz": 0.002352,
+        "I_xx": 0.001219,
+        "mass": 0.175,
+    }
+    for k, v in d.items():
+        np.testing.assert_equal(getattr(e, k), v, k)
+    np.testing.assert_allclose(e.diameter, 0.273049106904806)
