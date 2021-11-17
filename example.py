9ef80d45a60680ff960ca966e2e800ae8d662f77
Tom McClintock <tmcclintock89@gmail.com>
2021-11-16 22:50:28 -0500

    Show how a variable can be set at construction time in example.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 from frispy import Disc
 
-disc = Disc()
+disc = Disc(vx=10)
 result = disc.compute_trajectory()
 times = result.times
 x, y, z = result.x, result.y, result.z
